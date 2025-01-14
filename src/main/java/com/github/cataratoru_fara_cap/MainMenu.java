4d8a6fac3d673e132672895112000be8a5a4c0fc
VvladC <cimpoeruvlad7@gmail.com>
2025-01-14 11:15:09 +0200

    fix attempt failed

---
+++ src/main/java/com/github/cataratoru_fara_cap/MainMenu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.github.cataratoru_fara_cap;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+class MainMenu extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Main Menu");
+		primaryStage.show();
+	}
+}
